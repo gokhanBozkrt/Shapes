1c36fda8bedf55a4b84ad6807ffb5c31fbe9aa5c
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-05-04 21:52:54 +0300

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Gokhan Bozkurt on 4.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

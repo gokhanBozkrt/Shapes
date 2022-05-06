1dd13af73df9684ed9fcfd91519413d88260f9fc
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-05-06 14:14:56 +0300

    -Add Spirograph

--- Drawing/AnimatingComplex.swift
+++ Drawing/AnimatingComplex.swift
@@ -7,14 +7,58 @@
 
 import SwiftUI
 
+struct CheckerBoard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair <Double,Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+
+
 struct AnimatingComplex: View {
+    @State private var rows = 4
+    @State private var columns = 4
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CheckerBoard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
+                }
+            }
     }
 }
 
 struct AnimatingComplex_Previews: PreviewProvider {
     static var previews: some View {
         AnimatingComplex()
+            .preferredColorScheme(.dark)
     }
 }
